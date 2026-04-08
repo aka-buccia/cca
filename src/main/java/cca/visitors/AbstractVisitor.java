9f88e349f795b0bca517ed381547aa9a934b73f9
Filippo Bucciarelli <87319226+aka-buccia@users.noreply.github.com>
2026-04-08 16:31:08 +0200

    Add: visit methods for Procedure

--- src/main/java/cca/visitors/AbstractVisitor.java
+++ src/main/java/cca/visitors/AbstractVisitor.java
@@ -1,11 +1,17 @@
 package cca.visitors;
 
 import cca.Program;
+import cca.procedure.Procedure;
 
 public abstract class AbstractVisitor<T> implements VisitorInterface<T> {
 
     @Override
     public T visit(Program n) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public T visit(Procedure n) {
         throw new UnsupportedOperationException();
     }
 }
--- src/main/java/cca/visitors/VisitorInterface.java
+++ src/main/java/cca/visitors/VisitorInterface.java
@@ -1,9 +1,12 @@
 package cca.visitors;
 
 import cca.Program;
+import cca.procedure.Procedure;
 
 public interface VisitorInterface<R> {
 
     R visit(Program n);
+
+    R visit(Procedure n);
 
 }

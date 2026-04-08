af0e632137f972a8186fd677471f1dc9308d93eb
Filippo Bucciarelli <87319226+aka-buccia@users.noreply.github.com>
2026-04-08 15:54:06 +0200

    Add: Program node class
    Extension of Node. It's the starting point for visitors

---
+++ src/main/java/cca/Program.java
@@ -0,0 +1,21 @@
+package cca;
+
+import java.util.List;
+
+import cca.visitors.VisitorInterface;
+
+public class Program extends Node {
+
+    private final List<Procedure> procedures;
+
+    public Program(List<Procedure> procedures, Position position) {
+        super(position);
+        this.procedures = procedures;
+    }
+
+    @Override
+    public <R> R accept(VisitorInterface<R> v) {
+        return v.visit(this);
+    }
+
+}

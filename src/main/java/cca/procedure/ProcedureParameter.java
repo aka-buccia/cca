412e1ffcb44dbe5d2d7eb02f6b6ea7cb8d1339d0
Filippo Bucciarelli <87319226+aka-buccia@users.noreply.github.com>
2026-04-08 16:53:07 +0200

    Add: ProcedureParameter abstract node

---
+++ src/main/java/cca/procedure/ProcedureParameter.java
@@ -0,0 +1,11 @@
+package cca.procedure;
+
+import cca.Node;
+import cca.Position;
+
+public abstract class ProcedureParameter extends Node {
+
+    protected ProcedureParameter(Position position) {
+        super(position);
+    }
+}

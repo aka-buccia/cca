f98d0f18ea1c54cb6a1499b68819527b7773cae9
Filippo Bucciarelli <87319226+aka-buccia@users.noreply.github.com>
2026-04-09 12:26:13 +0200

    Add: Expression abstract node

---
+++ src/main/java/cca/expression/Expression.java
@@ -0,0 +1,11 @@
+package cca.expression;
+
+import cca.Node;
+import cca.Position;
+
+public abstract class Expression extends Node {
+
+    public Expression(Position position) {
+        super(position);
+    };
+}

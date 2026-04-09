01c3c078a9936af65bc93dd83fdadf842cce2b82
Filippo Bucciarelli <87319226+aka-buccia@users.noreply.github.com>
2026-04-09 12:18:46 +0200

    Add: Interaction abstract node

---
+++ src/main/java/cca/interaction/Interaction.java
@@ -0,0 +1,11 @@
+package cca.interaction;
+
+import cca.Node;
+import cca.Position;
+
+public abstract class Interaction extends Node {
+
+    public Interaction(Position position) {
+        super(position);
+    }
+}

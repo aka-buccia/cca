d4c5bde4dde75be2f456b61d349a303cab3acdb1
Filippo Bucciarelli <87319226+aka-buccia@users.noreply.github.com>
2026-04-29 17:38:09 +0200

    Add: Request node

---
+++ src/main/java/cca/interaction/Request.java
@@ -0,0 +1,33 @@
+package cca.interaction;
+
+import cca.expression.*;
+import cca.Position;
+import cca.visitors.VisitorInterface;
+import cca.Role;
+
+public class Request extends Interaction {
+
+    private final Expression sourceExpression;
+    private final Role sourceRole;
+    private final Media media;
+    private final Variable targetVariable;
+    private final Role targetRole;
+
+    public Request(Expression sourceExpression, Role sourceRole, Media media, Variable targetVariable,
+            Role targetRole,
+            Position position) {
+        this.sourceExpression = sourceExpression;
+        this.sourceRole = sourceRole;
+        this.media = media;
+        this.targetVariable = targetVariable;
+        this.targetRole = targetRole;
+
+        super(position);
+    }
+
+    @Override
+    public <R> R accept(VisitorInterface<R> v) {
+        return v.visit(this);
+    }
+
+}

1678d4b841503ee47311b7b217c5a82c9f3225aa
Filippo Bucciarelli <87319226+aka-buccia@users.noreply.github.com>
2026-04-30 11:10:15 +0200

    Add: Selection node

---
+++ src/main/java/cca/interaction/Selection.java
@@ -0,0 +1,39 @@
+package cca.interaction;
+
+import cca.Position;
+import cca.Role;
+import cca.Label;
+import cca.visitors.VisitorInterface;
+
+public class Selection extends Interaction {
+
+    private final Role sourceRole;
+    private final Role targetRole;
+    private final Label label;
+
+    public Selection(Role sourceRole, Role targetRole, Label label, Position position) {
+
+        this.sourceRole = sourceRole;
+        this.targetRole = targetRole;
+        this.label = label;
+        super(position);
+    }
+
+    public Role sourceRole() {
+        return this.sourceRole;
+    }
+
+    public Role targetRole() {
+        return this.targetRole;
+    }
+
+    public Label label() {
+        return this.label;
+    }
+
+    @Override
+    public <R> R accept(VisitorInterface<R> v) {
+        v.visit(this);
+    }
+
+}

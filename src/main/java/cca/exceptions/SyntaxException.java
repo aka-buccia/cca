715ed9f0508d06bd88206fc1d2e7bc45cdebfd16
Filippo Bucciarelli <87319226+aka-buccia@users.noreply.github.com>
2026-04-30 11:29:25 +0200

    Fix: better error message

--- src/main/java/cca/exceptions/SyntaxException.java
+++ src/main/java/cca/exceptions/SyntaxException.java
@@ -7,7 +7,7 @@ public class SyntaxException extends RuntimeException {
     private final Position position;
 
     public SyntaxException(Position position, String message) {
-        super(String.format("Syntax error at %s: %s", position, message));
+        super(String.format("Error at %s: %s", position, message));
         this.position = position;
     }
 

314dbdace8f1075f5683a4e37fedec3d2eb6a2e1
Filippo Bucciarelli <87319226+aka-buccia@users.noreply.github.com>
2026-04-30 11:08:33 +0200

    Add: label node

---
+++ src/main/java/cca/Label.java
@@ -0,0 +1,31 @@
+package cca;
+
+import cca.visitors.VisitorInterface;
+
+public class Label extends Node {
+
+    private final String id;
+
+    public Label(String id, Position position) {
+        this.id = id;
+        super(position);
+    }
+
+    public String id() {
+        return this.id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o instanceof Label) {
+            return this.id.equals(((Label) o).id());
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public <R> R accept(VisitorInterface<R> v) {
+        v.visit(this);
+    }
+}

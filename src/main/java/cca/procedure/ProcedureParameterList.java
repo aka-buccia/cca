8417b3043e0c3b6f280a5b39922aa4b53450700f
Filippo Bucciarelli <87319226+aka-buccia@users.noreply.github.com>
2026-05-08 17:18:53 +0200

    Add: ProcedureParameterList node

--- src/main/java/cca/procedure/ProcedureParameterList.java
+++ src/main/java/cca/procedure/ProcedureParameterList.java
@@ -1,7 +1,6 @@
 package cca.procedure;
 
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
 
 import cca.Node;
@@ -11,26 +10,28 @@ import cca.visitors.VisitorInterface;
 public class ProcedureParameterList extends Node {
 
     private final List<ProcedureParameter> parameters;
-    private final HashSet<StatefulParameter> statefulParameters;
-    private final HashSet<NonTerminatingParameter> nonTerminatingParameters;
-    private final HashSet<TerminatingParameter> terminatingParameters;
+    private final List<StatefulParameter> statefulParameters;
+    private final List<NonTerminatingParameter> nonTerminatingParameters;
+    private final List<TerminatingParameter> terminatingParameters;
 
-    public ProcedureParameterList(HashSet<StatefulParameter> statefulParameters,
-            HashSet<NonTerminatingParameter> nonTerminatingParameters,
-            HashSet<TerminatingParameter> terminatingParameters, Position position) {
+    public ProcedureParameterList(List<StatefulParameter> statefulParameters,
+            List<NonTerminatingParameter> nonTerminatingParameters,
+            List<TerminatingParameter> terminatingParameters, Position position) {
 
         this.statefulParameters = statefulParameters;
         this.nonTerminatingParameters = nonTerminatingParameters;
         this.terminatingParameters = terminatingParameters;
         super(position);
-        fillParametersList();
+        this.parameters = createParametersList();
     }
 
-    private void fillParametersList() {
-        this.parameters = new ArrayList<>();
+    private List<ProcedureParameter> createParametersList() {
+        List<ProcedureParameter> newParameterList = new ArrayList<>();
         this.parameters.addAll(this.statefulParameters);
         this.parameters.addAll(this.nonTerminatingParameters);
         this.parameters.addAll(this.terminatingParameters);
+
+        return newParameterList;
     }
 
     @Override

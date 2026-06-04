6f6484201faec63290b863185c21db8d9fcd1312
Filippo Bucciarelli <87319226+aka-buccia@users.noreply.github.com>
2026-06-04 11:06:15 +0200

    Add: antlr error listener

---
+++ src/main/java/cca/parser/ParsingErrorListener.java
@@ -0,0 +1,41 @@
+package cca.parser;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.antlr.v4.runtime.BaseErrorListener;
+import org.antlr.v4.runtime.RecognitionException;
+import org.antlr.v4.runtime.Recognizer;
+
+import cca.ast.Position;
+import cca.exceptions.SyntaxException;
+
+public class ParsingErrorListener extends BaseErrorListener {
+
+    private final String file;
+    private final List<SyntaxException> errors;
+
+    public ParsingErrorListener(String file) {
+        this.file = file;
+        this.errors = new ArrayList<>();
+    }
+
+    public List<SyntaxException> getErrors() {
+        return this.errors;
+    }
+
+    @Override
+    public void syntaxError(
+            Recognizer<?, ?> recognizer,
+            Object offendingSymbol,
+            int line,
+            int charPositionInLine,
+            String msg,
+            RecognitionException e) {
+
+        String file = recognizer.getInputStream().getSourceName().equals("<unknown>")
+                ? this.file
+                : recognizer.getInputStream().getSourceName();
+        errors.add(new SyntaxException(new Position(file, line, charPositionInLine), msg));
+    }
+}

3cd96a40cf418f605a0d538920946327a94306a8
Filippo Bucciarelli <87319226+aka-buccia@users.noreply.github.com>
2026-06-04 11:31:26 +0200

    Add: Parser

---
+++ src/main/java/cca/parser/Parser.java
@@ -0,0 +1,40 @@
+package cca.parser;
+
+import cca.FaaSChalCoreLexer;
+import cca.FaaSChalCoreParser;
+import cca.ast.Program;
+import cca.exceptions.CompoundException;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.antlr.v4.runtime.*;
+
+public class Parser {
+
+    private static Program parse(String fileName, CharStream input) {
+        FaaSChalCoreLexer lexer = new FaaSChalCoreLexer(input);
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+        FaaSChalCoreParser parser = new FaaSChalCoreParser(tokens);
+
+        parser.removeErrorListeners();
+        ParsingErrorListener errorListener = new ParsingErrorListener(fileName);
+        parser.addErrorListener(errorListener);
+
+        FaaSChalCoreParser.ProgramContext ctx = parser.program();
+        AstOptimizer optimizer = new AstOptimizer();
+
+        if (errorListener.getErrors().isEmpty()) {
+            return optimizer.visitProgram(ctx);
+        } else {
+            throw new CompoundException(errorListener.getErrors());
+        }
+
+    }
+
+    public static Program parseSourceFile(File file) throws IOException {
+        String filename = file.getCanonicalPath();
+        CharStream input = CharStreams.fromFileName(filename);
+        return parse(filename, input);
+    }
+}

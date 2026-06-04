464030497f4b38aa8e2c0898f4860b6db9fa4c4e
Filippo Bucciarelli <87319226+aka-buccia@users.noreply.github.com>
2026-06-04 11:06:52 +0200

    Add: faaschalcore runtime exception

---
+++ src/main/java/cca/exceptions/FaaSChalCoreException.java
@@ -0,0 +1,11 @@
+package cca.exceptions;
+
+public class FaaSChalCoreException extends RuntimeException {
+
+    public FaaSChalCoreException() {
+    }
+
+    public FaaSChalCoreException(String message) {
+        super(message);
+    }
+}

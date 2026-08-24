a3d145074c40ec68303bcc3b2f0184741b11e985
Filippo Bucciarelli <87319226+aka-buccia@users.noreply.github.com>
2026-08-25 00:26:48 +0200

    Refactor: use Set instead of List for terminationOrder

--- src/main/java/cca/checker/ProcedureSignature.java
+++ src/main/java/cca/checker/ProcedureSignature.java
@@ -1,9 +1,11 @@
 package cca.checker;
 
 import cca.ast.procedure.ProcedureParameterList;
-import cca.ast.procedure.TerminationOrder;
+import cca.ast.procedure.OrderingCouple;
+
+import java.util.Set;
 
 public record ProcedureSignature(
         ProcedureParameterList parameterList,
-        TerminationOrder terminationOrder) {
+        Set<OrderingCouple> terminationOrder) {
 }

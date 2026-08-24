775e370363f0cea6cac0751c4a0de637e3f290cd
Filippo Bucciarelli <87319226+aka-buccia@users.noreply.github.com>
2026-08-24 23:48:52 +0200

    Refactor: use TerminationOrderUtils method for transitiveClosure

--- src/main/java/cca/checker/CheckerContext.java
+++ src/main/java/cca/checker/CheckerContext.java
@@ -125,45 +125,7 @@ public class CheckerContext {
     }
 
     public void computeTerminationOrderTransitiveClosure() {
-        terminationOrder = computeTransitiveClosure(terminationOrder);
-    }
-
-    public Set<OrderingCouple> computeTransitiveClosure(Set<OrderingCouple> startingOrderSet) {
-        Set<OrderingCouple> closure = new HashSet<>(startingOrderSet);
-
-        boolean added;
-
-        do {
-            added = false;
-            Set<OrderingCouple> toAdd = new HashSet<>();
-
-            // For each couple (a, b) and (c, d)
-            for (OrderingCouple c1 : closure) {
-                for (OrderingCouple c2 : closure) {
-
-                    // If b == c
-                    if (c1.right().equals(c2.left())) {
-
-                        // Create (a, d)
-                        OrderingCouple newCouple = new OrderingCouple(
-                                c1.left(),
-                                c2.right(),
-                                c1.position());
-
-                        // If it hasn't been added yet, add it
-                        if (!closure.contains(newCouple) && !toAdd.contains(newCouple)) {
-                            toAdd.add(newCouple);
-                            added = true;
-                        }
-                    }
-                }
-            }
-
-            closure.addAll(toAdd);
-
-        } while (added);
-
-        return closure;
+        terminationOrder = TerminationOrderUtils.computeTransitiveClosure(terminationOrder);
     }
 
     public boolean isDefined(Role r) {

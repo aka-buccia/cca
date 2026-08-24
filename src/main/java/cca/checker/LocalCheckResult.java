4fbb89804dd78c61c8c758512b848bc102e86c94
Filippo Bucciarelli <87319226+aka-buccia@users.noreply.github.com>
2026-08-24 23:50:47 +0200

    Add: dto LocalCheckResult for results passing between Global and Local checker

---
+++ src/main/java/cca/checker/LocalCheckResult.java
@@ -0,0 +1,24 @@
+package cca.checker;
+
+import java.util.List;
+import java.util.Set;
+
+import cca.exceptions.IllFormedException;
+
+public class LocalCheckResult {
+    private final List<IllFormedException> errors;
+    private final Set<String> discoveredCalls;
+
+    public LocalCheckResult(List<IllFormedException> errors, Set<String> discoveredCalls) {
+        this.errors = errors;
+        this.discoveredCalls = discoveredCalls;
+    }
+
+    public List<IllFormedException> getErrors() {
+        return errors;
+    }
+
+    public Set<String> getDiscoveredCalls() {
+        return discoveredCalls;
+    }
+}

819dbdcbdf626ce42305ed5c2e23e9af297542ca
Filippo Bucciarelli <87319226+aka-buccia@users.noreply.github.com>
2026-08-28 12:23:17 +0200

    Refactor: IllFormedException extends AstPositionedException

--- src/main/java/cca/exceptions/IllFormedException.java
+++ src/main/java/cca/exceptions/IllFormedException.java
@@ -2,17 +2,8 @@ package cca.exceptions;
 
 import cca.ast.Position;
 
-public class IllFormedException extends FaaSChalCoreException {
-
-    private final Position position;
-
+public class IllFormedException extends AstPositionedException {
     public IllFormedException(Position position, String message) {
-        super(message);
-        this.position = position;
+        super(position, message);
     }
-
-    public Position getPosition() {
-        return position;
-    }
-
 }

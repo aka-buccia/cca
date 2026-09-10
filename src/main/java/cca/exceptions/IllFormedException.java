04f91644cc5dc7a832b53c1b6aaebd013495c0ae
Filippo Bucciarelli <87319226+aka-buccia@users.noreply.github.com>
2026-09-10 11:13:18 +0200

    Refactor: IllFormed and Syntax extend AstException

--- src/main/java/cca/exceptions/IllFormedException.java
+++ src/main/java/cca/exceptions/IllFormedException.java
@@ -2,7 +2,7 @@ package cca.exceptions;
 
 import cca.ast.Position;
 
-public class IllFormedException extends AstPositionedException {
+public class IllFormedException extends AstException {
     public IllFormedException(Position position, String message) {
         super(position, message);
     }
--- src/main/java/cca/exceptions/SyntaxException.java
+++ src/main/java/cca/exceptions/SyntaxException.java
@@ -2,7 +2,7 @@ package cca.exceptions;
 
 import cca.ast.Position;
 
-public class SyntaxException extends AstPositionedException {
+public class SyntaxException extends AstException {
     public SyntaxException(Position position, String message) {
         super(position, message);
     }
